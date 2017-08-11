6e61ba0bc08791776b81b715b29d6f4eac8fb9c1
Logan Absher <absherlogan@gmail.com>
2017-08-11 11:58:18 -0700

    testing scafolding

---
+++ test/error-middleware-test.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const expect = require('chai').expect;
+const request = require('superagent');
+const createError = require('http-errors');
+
+const errMiddleware = require('../lib/error-middleware.js');
+
+// const testErr = createError(400, 'test error');
+const url = `http://localhost:${process.env.PORT}`;
+require('../server.js');
+
+describe('Error Middleware', () => {
+  describe('with existing error status', () => {
+    it('', (done) => {
+
+    });
+  });
+
+  describe('with a name of ValidationError', () => {
+    it('', () => {
+
+    });
+  });
+
+  describe('with no error status', () => {
+    it('', () => {
+      // request.get(`${url}/api/not/a/route`, (err, res) => {
+      //   expect(err.status).to.equal();
+      //   done();
+      // })
+    });
+  });
+});
