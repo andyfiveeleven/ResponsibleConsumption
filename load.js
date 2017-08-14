36aba22fe513e34ef722e46874c9ed0ad363888b
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-08-14 15:41:12 -0700

    finished with load function

--- load.js
+++ load.js
@@ -8,7 +8,7 @@ module.exports = exports = {};
 
 exports.load = function(num) {
   debug('loading api');
-  for(let i = num; i != num+24; i++){
+  for(let i = num; i != num+19; i++){
     request.get(`https://www.cannabisreports.com/api/v1.0/edibles?page=${i}`)
     .set('X-API-Key', 'CANNABIS_REPORTS_API_KEY')
     .then( res => {
@@ -16,20 +16,10 @@ exports.load = function(num) {
     }).then( edible => {
       console.log('CHHHHHHEEEEESSSSSEEEEEE', edible.data.length);
       for(let i = 0; i < edible.data.length; i++){
+        console.log('THIS IS ADDDING THE OBJECT: ', edible.data[i]);
         new Edible(edible.data[i]).save();
       }
     });
   }
+  console.log('MIGRATION COMPLETE!');
 };
-
-
-// }
-// for(var i = 0; i <= 5; i++){
-
-//   request.get(res.body.meta.pagination.links.next)
-//   .set('X-API-Key', 'CANNABIS_REPORTS_API_KEY')
-//   .end((err, res) => {
-//     if(err) console.log(err.body);
-//     console.log('$$$$$$$$$$$$$$$$%%%%%%%%%%%%%', res.body);
-//   });
-// }
