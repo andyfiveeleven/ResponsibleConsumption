d5b4042e7f3fe45a282cb867bfff1ff5fd42be34
Logan Absher <absherlogan@gmail.com>
2017-08-15 16:27:53 -0700

    fixed multiple signin and signup routes

--- route/auth-route.js
+++ route/auth-route.js
@@ -13,10 +13,12 @@ const authRouter = module.exports = Router();
 
 authRouter.post('/api/signup', jsonParser, function (req, res, next) {
   debug('POST: /api/signup');
+  console.log('yes');
 
   if(!req.body.password) return next(createError(400, 'password required'));
   if(!req.body.username) return next(createError(400, 'username required'));
   if(!req.body.email) return next(createError(400, 'email required'));
+  console.log('yes');
 
   let password = req.body.password;
   delete req.body.password;
@@ -26,22 +28,35 @@ authRouter.post('/api/signup', jsonParser, function (req, res, next) {
   user.generatePasswordHash(password)
   .then((user) => user.save())
   .then((user) => user.generateToken())
-  .then((token) => res.send(token))
+  .then((token) => {
+    console.log(token);
+    // res.body = token;
+    res.json(token);
+  })
   .catch(next);
 });
 
 authRouter.get('/api/signin', basicAuth, function(req, res, next){
   debug('GET: /api/signin');
+  console.log('yes');
 
-  if(!req.username) return next(createError(400, 'username required'));
-  if(!req.password) return next(createError(400, 'password required'));
+  if(!req.username) next(createError(400, 'username required'));
+  if(!req.password) next(createError(400, 'password required'));
+  console.log('yes');
 
   User.findOne({username: req.auth.username})
   .then((user) => {
     if(!user) return Promise.reject(createError(401, 'invalid username'));
     return user.comparePasswordHash(req.auth.password);
   })
-  .then((user) => user.generateToken())
-  .then((token) => res.send(token))
+  .then((user) => {
+    console.log(user);
+    user.generateToken();
+  })
+  .then((token) => {
+    console.log(token);
+    // res.body = token;
+    res.json(token);
+  })
   .catch(next);
 });
