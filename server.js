5f5b5e046eefb2459a88598d12830accb4b680c0
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-08-14 17:55:25 -0700

    updated monday files

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 'use strict';
 
 const express = require('express');
-
-
-
-
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
@@ -14,7 +10,7 @@ const debug = require('debug')('credibleEdibles:server');
 const errors = require('./lib/error-middleware.js');
 const signup = require('./route/signup-route');
 const signin = require('./route/signin-route');
-const profile = require('./route/profile-route.js')
+const profile = require('./route/profile-route.js');
 const edibleRouter = require('./route/edible-route.js');
 const load = require('./lib/load.js');
 
@@ -35,4 +31,3 @@ app.use(edibleRouter);
 app.use(errors);
 
 app.listen(PORT, () => debug(`app listening on: ${PORT}`));
-
---
+++ test/edible-route-test.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const expect = require('chai').expect;
+const request = require('superagent');
