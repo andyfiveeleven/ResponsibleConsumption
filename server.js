c1ffd4d1836b2e3d2fd8141aca8ac2277131bcad
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-08-16 10:37:22 -0700

    replaced server.js because I deleted it

---
+++ server.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
+const debug = require('debug')('credibleEdibles:server');
+
+const errors = require('./lib/error-middleware.js');
+const profile = require('./route/profile-route.js');
+const expReview = require('./route/exp-review-route.js');
+const edibleRouter = require('./route/edible-route.js');
+const load = require('./lib/load.js');
+
+dotenv.load();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true });
+
+app.use(cors());
+app.use(morgan('dev'));
+
+
+app.use(profile);
+app.use(expReview);
+app.use(edibleRouter);
+load.load(280);
+app.use(errors);
+
+app.listen(PORT, () => debug(`app listening on: ${PORT}`));
