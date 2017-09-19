f8c739b65a2d44359f545540c83c6ef27e4ef227
JohnScarrow <johna@scarrows.com>
2017-09-18 18:02:30 -0700

    Revert "James reco staging"

--- route/comment-route.js
+++ route/comment-route.js
@@ -5,27 +5,20 @@ const jsonParser = require('body-parser').json();
 const debug = require('debug')('credibleEdibles:comment-router');
 const Edible = require('../model/edible.js');
 const Comment = require('../model/comment.js');
-const User = require('../model/user.js');
 const bearerAuth = require('../lib/bearer-auth-middleware.js');
 
 const commentRouter = module.exports = new Router();
 
 
-commentRouter.post('/api/edible/:edibleID/:userID/comment', bearerAuth, jsonParser, function(req, res, next) {
+commentRouter.post('/api/edible/:edibleID/comment', bearerAuth, jsonParser, function(req, res, next) {
   debug('POST: /api/edible/:edibleID/comment');
-  let commentRating = Comment.generateOverAllRating(req.body)
-  Edible.findByIdAndAddComment(req.params.edibleID, commentRating)
+
+  Edible.findByIdAndAddComment(req.params.edibleID, req.body)
   .then( comment => {
-    return comment
+    res.json(comment);
   })
-  .then((comment) => {
-    User.findByIdAndAddComment(req.params.userID, comment)
-    .then( comment => {
-      res.json(comment);
-    })
-    .catch(next);
-  })
-})
+  .catch(next);
+});
 
 commentRouter.get('/api/comment/:id', bearerAuth, function(req, res, next) {
   debug('GET: /api/comment/:id');
@@ -51,11 +44,7 @@ commentRouter.delete('/api/comment/:id', bearerAuth, function(req, res, next) {
   debug('DELETE: /api/comment/:id');
 
   Comment.findById(req.params.id)
-  .then((comment) => {
-    Edible.findByIdAndRemoveComment(comment.edibleID, req.params.id)
-    return comment
-  })
-  .then((comment) => User.findByIdAndRemoveComment(comment.userid, req.params.id))
+  .then((comment) => Edible.findByIdAndRemoveComment(comment.edibleID, req.params.id))
   .then(() => Comment.findByIdAndRemove(req.params.id))
   .then(() => res.status(204).send())
   .catch(next);
